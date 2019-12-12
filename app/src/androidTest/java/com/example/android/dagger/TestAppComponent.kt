0bd9afedb5dfbcc8d49e66ebeb1950716bfd9d73
Sergey Eshin <sergey.eshin@dataart.com>
2019-12-12 16:56:13 +0300

    added test coverage

---
+++ app/src/androidTest/java/com/example/android/dagger/TestAppComponent.kt
@@ -0,0 +1,10 @@
+package com.example.android.dagger
+
+import com.example.android.dagger.di.AppComponent
+import com.example.android.dagger.di.AppSubcomponents
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [TestStorageModule::class, AppSubcomponents::class])
+interface TestAppComponent : AppComponent
